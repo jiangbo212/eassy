9d1d01936e257095b082143f67fd80cfef2c86f4
jiangbo212 <peiyaoliukuan@126.com>
2020-06-08 09:28:28 +0800

    add annotate

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ import (
     		redis.call('incr', KEYS[2])
     		return 1
   		end
- */
+*/
 const PhoneLotteryLua = "local all_count_str = redis.call('exists', KEYS[1]) local cur_count_str = redis.call('exists', KEYS[2]) local all_count = tonumber(all_count_str) local cur_count = tonumber(cur_count_str) if all_count ==0 then redis.call('set', KEYS[1], 0) end if cur_count > 0 then return 0 end local all_count_value_str = redis.call('get', KEYS[1]) local all_count_value = tonumber(all_count_value_str) local max_count = tonumber(ARGV[1]) if all_count_value >= max_count then return 0 else redis.call('incr', KEYS[1]) redis.call('incr', KEYS[2]) return 1 end"
 
 /*
@@ -62,37 +62,40 @@ const CardLotteryLua = "local all_count_str = redis.call('exists', KEYS[1]) loca
 /*
 	phoneLotteryKey, 手机抽奖的已发放手机奖品的key
 	cardLotteryKey, 电话卡抽奖的已发放电话卡奖品的key
- */
+*/
 const phoneLotteryKey, cardLotteryKey = "phone_lottery_amount", "card_lottery_amount"
 
 /*
 	phoneMaxCount, 手机奖品的最大数量
 	cardMaxCount，电话卡奖品的最大数量
 	cardUserMaxCount, 单个用户可获取到的电话卡奖品的最大数量
- */
+*/
 const phoneMaxCount, cardMaxCount, cardUserMaxCount = "5", "100", "2"
 
 /*
 	captchaQueue, 验证码消息队列
 	registerQueue, 注册信息消息队列
- */
-const captchaQueue, registerQueue, lotteryQueue  = "CAPTCHA_QUEUE", "REGISTER_QUEUE", "LOTTERY_QUEUE"
+*/
+const captchaQueue, registerQueue, lotteryQueue = "CAPTCHA_QUEUE", "REGISTER_QUEUE", "LOTTERY_QUEUE"
 
 /*
 	手机号正则表达式
- */
+*/
 const regepPhoneNumber = `^1[3-9]\d{9}$`
 
 /*
 	日期格式化字符串
- */
+*/
 const dateFormat = "20060102"
 
 /*
 	sqlite db location
- */
+*/
 const sqliteDbLocation = "./test.db"
 
+/*
+	创建注册信息表
+*/
 const registerSql = `
 	create table main."register"
      (
@@ -103,7 +106,10 @@ const registerSql = `
      )
 `
 
-const lotterySql  = `
+/*
+	创建中奖记录表
+*/
+const lotterySql = `
    create table main."lottery"
 	(
 		id          INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -114,14 +120,23 @@ const lotterySql  = `
 	)
 `
 
+/*
+	注册信息表增加手机号作为索引
+*/
 const registerIndex = "create index register_phone on register(phone)"
+
+/*
+	中奖信息表增加手机号作为索引
+*/
 const lotteryIndex = "create index lottery_phone on lottery(phone)"
 
 var redisClient = redis.NewClient(&redis.Options{
-	Addr:"127.0.0.1:6379",
+	Addr: "127.0.0.1:6379",
 })
 
-// 发送验证码
+/*
+	发送验证码
+*/
 func captchaHandler(w http.ResponseWriter, r *http.Request) {
 	var captchaCode = "666666"
 
@@ -129,14 +144,14 @@ func captchaHandler(w http.ResponseWriter, r *http.Request) {
 
 	result, error := regexp.Match(regepPhoneNumber, []byte(phoneNumber))
 
-	if len(phoneNumber) ==0 || error != nil || !result {
+	if len(phoneNumber) == 0 || error != nil || !result {
 		fmt.Fprint(w, "手机号不合法")
 		return
 	}
 
-	val, err := redisClient.SetNX(redisClient.Context(), phoneNumber, captchaCode, 60 * time.Second).Result()
+	val, err := redisClient.SetNX(redisClient.Context(), phoneNumber, captchaCode, 60*time.Second).Result()
 
-	if err !=nil {
+	if err != nil {
 		fmt.Println("connect redis server error", err)
 		fmt.Fprint(w, "系统异常")
 		return
@@ -152,22 +167,26 @@ func captchaHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, captchaCode)
 }
 
-// 发送验证码
-func sendCaptcha( captcha string)  {
+/*
+	验证码消息入队列
+*/
+func sendCaptcha(captcha string) {
 	result := redisClient.LPush(redisClient.Context(), captchaQueue, captcha).Val()
 	if result > 0 {
 		fmt.Println("验证码消息发送成功", captcha)
 	}
 }
 
-// 用户注册
+/*
+	用户注册
+*/
 func registerHandler(w http.ResponseWriter, r *http.Request) {
 
 	var captcha, phoneNumber, content = r.PostFormValue("captcha"), r.PostFormValue("phoneNumber"), r.PostFormValue("content")
 
 	redisCaptcha := redisClient.Get(redisClient.Context(), phoneNumber).Val()
 
-	if len(redisCaptcha) ==0 || captcha != redisCaptcha {
+	if len(redisCaptcha) == 0 || captcha != redisCaptcha {
 		fmt.Fprint(w, "验证码错误")
 		return
 	}
@@ -177,15 +196,19 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "register success")
 }
 
-// 发送注册消息
-func sendRegisterMessage(phoneNumber string, content string)  {
-	result := redisClient.LPush(redisClient.Context(), registerQueue, phoneNumber + "," + content).Val()
+/*
+	注册消息入队列
+*/
+func sendRegisterMessage(phoneNumber string, content string) {
+	result := redisClient.LPush(redisClient.Context(), registerQueue, phoneNumber+","+content).Val()
 	if result > 0 {
 		fmt.Println("注册消息发送成功", phoneNumber, content)
 	}
 }
 
-// 用户抽奖
+/*
+	用户抽奖
+*/
 func lotteryHandler(w http.ResponseWriter, r *http.Request) {
 
 	curDay := time.Now().Format(dateFormat)
@@ -194,9 +217,9 @@ func lotteryHandler(w http.ResponseWriter, r *http.Request) {
 
 	var lotteryKey = phoneNumber + "_" + curDay
 
-	lotteryValue, error := redisClient.SetNX(redisClient.Context(), lotteryKey, "1", 24 * time.Hour).Result()
+	lotteryValue, error := redisClient.SetNX(redisClient.Context(), lotteryKey, "1", 24*time.Hour).Result()
 
-	if error !=nil {
+	if error != nil {
 		fmt.Fprint(w, "系统异常, 请稍后重试")
 		return
 	}
@@ -212,17 +235,16 @@ func lotteryHandler(w http.ResponseWriter, r *http.Request) {
 	var phoneLotteryCurDay, cardLotteryUserNo = "phone_lottery_" + curDay, "card_lottery_" + phoneNumber
 
 	var phoneLotteryKeys = []string{phoneLotteryKey, phoneLotteryCurDay}
-	var phoneLotteryArgs = []string {phoneMaxCount}
+	var phoneLotteryArgs = []string{phoneMaxCount}
 
 	var cardLotteryKeys = []string{cardLotteryKey, cardLotteryUserNo}
 	var cardLotterArgs = []string{cardMaxCount, cardUserMaxCount}
 
-
 	switch result {
 	case 1:
-		val, err :=redisClient.Eval(redisClient.Context(), PhoneLotteryLua, phoneLotteryKeys, phoneLotteryArgs).Result()
+		val, err := redisClient.Eval(redisClient.Context(), PhoneLotteryLua, phoneLotteryKeys, phoneLotteryArgs).Result()
 
-		if err !=nil {
+		if err != nil {
 			fmt.Fprint(w, "系统异常, 请稍后重试")
 			return
 		}
@@ -239,7 +261,7 @@ func lotteryHandler(w http.ResponseWriter, r *http.Request) {
 	case 2:
 		val, err := redisClient.Eval(redisClient.Context(), CardLotteryLua, cardLotteryKeys, cardLotterArgs).Result()
 
-		if err !=nil {
+		if err != nil {
 			fmt.Fprint(w, "系统异常, 请稍后重试")
 			return
 		}
@@ -262,22 +284,28 @@ func lotteryHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "获奖奖品为贴纸")
 }
 
-func sendLotteryMessage(phoneNumber string, rel int, curDay string)  {
+/*
+	抽奖成功消息入队列
+*/
+func sendLotteryMessage(phoneNumber string, rel int, curDay string) {
 	result := redisClient.LPush(redisClient.Context(), lotteryQueue, fmt.Sprintf("%s,%d,%s", phoneNumber, rel, curDay)).Val()
 	if result > 0 {
 		fmt.Println("获奖消息发送成功")
 	}
 }
 
-func lotteryResult(phoneNumber string) int  {
+/*
+	中奖逻辑
+*/
+func lotteryResult(phoneNumber string) int {
 
 	// 手机
-	if phoneNumber == "17898875239" {
+	if strings.Contains(phoneNumber, "239") {
 		return 1
 	}
 
 	// 电话卡
-	if phoneNumber == "18817500884" {
+	if strings.Contains(phoneNumber, "883") {
 		return 2
 	}
 
@@ -287,8 +315,8 @@ func lotteryResult(phoneNumber string) int  {
 
 /*
 	监听验证码消息并发送
- */
-func onCaptChaMessage()  {
+*/
+func onCaptChaMessage() {
 	for {
 		captchaMessage := redisClient.LPop(redisClient.Context(), captchaQueue).Val()
 
@@ -299,8 +327,8 @@ func onCaptChaMessage()  {
 }
 
 /*
-	处理注册成功消息
- */
+	监听注册成功消息
+*/
 func onRegisterMessage() {
 	for {
 		registerMessage := redisClient.LPop(redisClient.Context(), registerQueue).Val()
@@ -314,7 +342,7 @@ func onRegisterMessage() {
 }
 
 /*
-	处理抽奖成功消息
+	监听抽奖成功消息
 */
 func onLotteryMessage() {
 	for {
@@ -328,19 +356,22 @@ func onLotteryMessage() {
 	}
 }
 
+/*
+	写入注册数据及中奖数据
+*/
 func insertData(insertSql string, args []string) {
 	db := getConnection()
 	stmt, err1 := db.Prepare(insertSql)
-	if err1 !=nil {
+	if err1 != nil {
 		fmt.Println("写入数据库异常", err1)
 	}
 
 	var err = errors.New("init")
-	if len(args) ==2 {
+	if len(args) == 2 {
 		_, err = stmt.Exec(args[0], args[1])
 	}
 
-	if len(args) ==3 {
+	if len(args) == 3 {
 		_, err = stmt.Exec(args[0], args[1], args[2])
 	}
 
@@ -350,24 +381,30 @@ func insertData(insertSql string, args []string) {
 	db.Close()
 }
 
+/*
+	获取数据库连接
+*/
 func getConnection() *sql.DB {
 	db, err := sql.Open("sqlite3", sqliteDbLocation)
-	if err !=nil {
+	if err != nil {
 		fmt.Println("创建sqlite连接异常", err)
 		os.Exit(1)
 	}
 	return db
 }
 
-func initDatabase()  {
+/*
+	初始化数据库配置
+*/
+func initDatabase() {
 	os.Create(sqliteDbLocation)
 
 	db := getConnection()
 
 	for _, v := range []string{registerSql, lotterySql, registerIndex, lotteryIndex} {
 		_, err := db.Exec(v)
-		if err !=nil {
-			fmt.Println("sql执行异常", v, err);
+		if err != nil {
+			fmt.Println("sql执行异常", v, err)
 			os.Exit(1)
 		}
 	}
